aa263e2864236cfef49d3e79199ca07a59ee583d
agrwl-prankit <agrwnlprankit77@gmail.com>
2024-06-10 13:03:39 +0530

    rendering multi dimensional list

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 let initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,12 +7,23 @@ let initialGameBoard = [
 ]
 
 export default function GameBoard(){
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex) {
+        setGameBoard((prevGameBoard) => {
+            const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updateBoard[rowIndex][colIndex] = 'X';
+            return updateBoard;
+        });
+    }
+
     return (
         <ol id='game-board'>
-            {initialGameBoard.map((row, rowIndex) => <li key={rowIndex}>
+            {gameBoard.map((row, rowIndex) => <li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => <li key={colIndex}>
-                        <button>{playerSymbol}</button>
+                        <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                            {playerSymbol}</button>
                     </li>)}
                 </ol>
             </li>)}
